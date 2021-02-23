7d947c82d37f700de30be0599fd1327c41877fad
unknown <tejus423@gmail.com>
2021-02-23 17:46:01 -0600

    added metallic surfaces and dielectrics to the material section

--- vec3.h
+++ vec3.h
@@ -66,6 +66,15 @@ class vec3  {
             return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
         }
 
+        bool near_zero() const 
+        {
+            // return true if the vector is close to zero amongst its dimensions
+            // we want to know because any operation done with a near zero can result in NaN errors
+            const auto s = 1e-8;
+            return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);   
+        }
+
+
     public:
         float e[3];
 };
@@ -207,6 +216,21 @@ vec3 random_in_hemisphere(const vec3& normal)
     {
         return -in_unit_sphere;
     }
+}
+
+// reflection function on metal surfaces when the ray will scatter
+vec3 reflect(const vec3& v, const vec3& n)
+{
+    return v - 2*dot(v,n)*n;
+}
+
+// refraction function for dielectric materials
+vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat)
+{
+    auto cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp = etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.squared_length())) * n;
+    return r_out_perp + r_out_parallel;
 }
 
 #endif
